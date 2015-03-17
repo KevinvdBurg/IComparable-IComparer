e751c0c50637167efbd24c2e1296c87ce0255735
KevinvdBurg <vanderburg.kevin@gmail.com>
2015-03-17 13:11:54 +0100

    Create Diagram

---
+++ IComparable-IComparer/IComparable-IComparer/Student.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IComparable_IComparer
+{
+    public class Student
+    {
+        public Student(string name, string address, int number)
+        {
+            throw new System.NotImplementedException();
+        }
+    
+        public string Name
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public string Address
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public int Number
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public string ToString()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
